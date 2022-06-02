31103a2180a8d1336dfb9084825d8503199fbf01
Daniel Gutierrez Agaton <l21211958@tectijuana.edu.mx>
2022-06-02 11:09:28 -0700

    Versión 4.0

--- Escritura-Archivos/Program.cs
+++ Escritura-Archivos/Program.cs
@@ -9,42 +9,55 @@ namespace Escritura_Archivos
 {
     class Program
     {
-       
-      
+       class  Datos
+       {
+           public string Nom;
+            public   int Edad;
+            public string Pelicula;
+            public string Musica;
 
+
+            public Datos(string Nom, int Edad, string Pelicula, string Musica)
+            {
+                this.Nom = Nom;
+                this.Edad = Edad;
+                this.Pelicula = Pelicula;
+                this.Musica = Musica;
+            }
+            
+          
+            
+        }
+    
         static void Main(string[] args)
         {
             string Nom;
-            string Edad;
+            int Edad;
             string Pelicula;
+            string Musica;
 
             Console.Write("Dame un nombre con apellidos: ");
             Nom = Convert.ToString(Console.ReadLine());
             Console.Write("Dame tu edad: ");
-            Edad = Convert.ToString(Console.ReadLine());
+            Edad = Convert.ToInt32(Console.ReadLine());
             Console.Write("Dame tu Pelicula de favorita: ");
             Pelicula = Convert.ToString(Console.ReadLine());
+            Console.Write("Dame tu Musica de favorita: ");
+            Musica = Convert.ToString(Console.ReadLine());
 
 
             StreamWriter sw = new StreamWriter("Ejemplo.txt",true);
+            Datos D = new Datos(Nom, Edad, Pelicula, Musica);
             //si el archivo no existe lo creara
             //si ya existe, escribira en el
 
-            
-            string[] Lines ={
-                "Datos de la persona",
-                 "El nombre es: " +  Nom,
-                "la edada es: " + Edad,
-               "Su pelicula favorita: " + Pelicula
-             };
+            sw.WriteLine("Datos de la persona");
+            sw.WriteLine("El nombre es: " + D.Nom);
+            sw.WriteLine("la edada es: " + D.Edad);
+            sw.WriteLine("Su pelicula favorita: " + D.Pelicula);
+            sw.WriteLine("Su Musica favorita: " + D.Musica);
 
-           
-         
-            foreach (string Line in Lines)
-            {
-                sw.WriteLine(Line);
-            }
-            sw.Close();//se debe cerrare el archivo
+            sw.Close();
 
             Console.WriteLine("Escrbiendo en el archivo.....");
             Console.ReadLine();
